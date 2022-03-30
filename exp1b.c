14bd5e534b5a0838243c31d867ce34dd948fdcb9
Sai Rane <sairane2828@gmail.com>
2022-03-30 22:04:47 +0530

    celcius to farenheit

---
+++ exp1b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	
+	float f,c;
+	printf("enter temprature in c:");
+	scanf("%f",&c);
+	f=(c*1.8)+32;
+	printf("temprature in farenhite is:%f",f);
+	getch();
+} 
