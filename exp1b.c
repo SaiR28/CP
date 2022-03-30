a27dc5f917af48feb3268b4f56b8f363df873042
Sai Rane <sairane2828@gmail.com>
2022-03-30 21:52:38 +0530

    basic calculator in c

---
+++ exp1b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	
+	float a,b,c;
+	printf("enter two numbers:");
+	scanf("%f %f",&a,&b);
+	c=a+b;
+	printf("value of addition is:%f\n",c);
+	printf("value of subtraction is:%f\n",a-b);
+	printf("value of multiplication is:%f\n",a*b);
+	printf("value of division:%f\n",a/b);
+	getch();
+} 
