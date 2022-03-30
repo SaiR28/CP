a99977c85d1e34643bf3b448693a92aed6a77b89
Sai Rane <sairane2828@gmail.com>
2022-03-30 21:35:58 +0530

    Create 1a.c

---
+++ 1a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	
+	printf("Name:\t Sai Rane\n");
+	printf("Roll no: 2114042\n");
+	printf("Address: Aquem\n");
+	printf("Hobbies: Gaming\n");
+	getch();
+} 
